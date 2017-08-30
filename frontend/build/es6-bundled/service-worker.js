678171516c85390e9aded4ca123616718fca9c15
Justin Huang <jstn@cs.washington.edu>
2017-08-29 17:21:54 -0700

    Updated build.

--- frontend/build/es6-bundled/service-worker.js
+++ frontend/build/es6-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","c92f9ed8f914d771d8f3425f3267a8f3"]];
+var precacheConfig = [["index.html","61250fd1c13c7fb19a5ffa81115f5941"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
